75b0dc6ce0a70ae07b83d6e458ddeb8961e60765
DoniyorShuxratov <shuxratovdoni@gmail.com>
2023-12-28 19:23:37 +0500

    update

--- src/pages/Home/OurProducts.jsx
+++ src/pages/Home/OurProducts.jsx
@@ -1,26 +1,35 @@
-import data from '../../data/data.json';
+import data from '../../data/data.json'
 import { TopRightArrow } from '../../icons/top-right-arrow';
+import { useNavigate } from 'react-router-dom';
+
+const products = data?.products || [];
+const lengthFourProduct = products.slice(0,4)
 
 export default function OurProduct() {
+    const navigate = useNavigate();
 
-    return (
+    const handleProductClick = (product) => {
+        navigate('/product-screen', {state: product});
+    }
+    return(
         <section className="intro--product--section">
             <div className="section-title container">
                 <h1>Наша продукция</h1>
                 <button>Смотреть все</button>
             </div>
             <div className="intro--product--container container">
-                {data?.products?.map((item, i)=>(
-                    <button key={i}>
+                {lengthFourProduct?.map((item, i) => (
+                    <button style={{backgroundImage: `linear-gradient(0deg, var(--primary-with-opacity) 0%, var(--primary-with-opacity) 50%), url(${item.imgSrc})`,}} key={i} className="intro--product--card" onClick={() => handleProductClick(item)}>
                         <div className="card--title">
                             <h3>{item.productName}</h3>
+                            <span>{item.productSubName}</span>
                         </div>
                         <div className="card--sub">
                             <TopRightArrow />
                         </div>
                     </button>
-                ))}   
+                ))}
             </div>
         </section>
-    );
+    )
 }
