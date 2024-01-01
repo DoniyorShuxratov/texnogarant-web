957ad16238469a2bdd3db51349bc2f41642efac5
DoniyorShuxratov <shuxratovdoni@gmail.com>
2024-01-01 21:13:22 +0500

    fix package

--- src/pages/Home/ContactUs.jsx
+++ src/pages/Home/ContactUs.jsx
@@ -32,7 +32,7 @@ export default function Contact() {
                 </div>
 
                 <div className="contact--map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11989.528492361833!2d69.2841786146164!3d41.30055005009436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1703222812339!5m2!1sru!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11989.528492361833!2d69.2841786146164!3d41.30055005009436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1703222812339!5m2!1sru!2s" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </section>
